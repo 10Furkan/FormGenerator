2dcbc49bb8d5891727808d34f8e179f59c90acbb
Furkan <furkanyilmaz1821@gmail.com>
2026-03-12 15:43:33 +0300

    Feat: .env structure and configuration added.

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection error: " . $e->getMessage());
+}
+?>
